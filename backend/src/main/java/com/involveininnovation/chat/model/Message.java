dc4583a41b6762f35aacb4a38726db69c9caefe7
sarathchandram <vm2251@nyit.edu>
2023-09-18 17:35:28 -0400

    Initial commit

---
+++ backend/src/main/java/com/involveininnovation/chat/model/Message.java
@@ -0,0 +1,37 @@
+package com.involveininnovation.chat.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public String getSenderName()
+	{
+		return senderName;
+	}
+	public String getMessage()
+	{
+		return message;
+	}
+	public String getDate()
+	{
+		return date;
+	}
+	public Status getStatus()
+	{
+		return status;
+	}
+}
